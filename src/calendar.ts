c872b764ea08fc68bbb6f546afc475ddf960a2dc
John Reilly <johnny_reilly@hotmail.com>
2021-08-07 09:40:49 +0100

    alternate approach

--- src/calendar.ts
+++ src/calendar.ts
@@ -4,7 +4,9 @@ import { getArgs, makeOAuth2Client } from "./shared";
 async function makeCalendarClient() {
   const { clientId, clientSecret, refreshToken } = await getArgs();
   const oauth2Client = makeOAuth2Client({ clientId, clientSecret });
-  oauth2Client.credentials.refresh_token = refreshToken;
+  oauth2Client.setCredentials({
+    refresh_token: refreshToken
+  });
 
   const calendarClient = google.calendar({
     version: "v3",
